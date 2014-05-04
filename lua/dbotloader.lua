bb539773ea30699b3f58a113c0fbeee71a9270ec
millerlogic <chris@cmiller.me>
2014-05-03 18:03:21 -0700

    Improve dbotloader run steps again.

--- lua/dbotloader.lua
+++ lua/dbotloader.lua
@@ -1,12 +1,19 @@
 
 
 --[[
+# Enter ludebot dir:
+cd ludebot
+
+# Values:
+export IRC_SERVER=server.com
+export IRC_NICK=testludebot
+
 # Setup paths:
-export LUA_PROJECTS=../..
+export LUA_PROJECTS=..
 export LUA_PATH="$LUA_PROJECTS/ludebot/lua/?.lua;$LUA_PROJECTS/irccmd/lua/?.lua;$LUA_PROJECTS/luasandy/lua/?.lua;;"
 
 # Run ludebot:
-$LUA_PROJECTS/irccmd/irccmd server.com testludebot "-input:RUN=$RUN {1+}" -load=$LUA_PROJECTS/ludebot/lua/dbotloader.lua
+$LUA_PROJECTS/irccmd/irccmd $IRC_SERVER $IRC_NICK "-input:RUN=$RUN {1+}" -load=$LUA_PROJECTS/ludebot/lua/dbotloader.lua
 # Add -flag=firstrun to create initial configs for the first run.
 --]]
 
